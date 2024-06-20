2fa8506b263fc15571e3923a83b756b7f709fe09
Karthik G. Appiah <karthikgappiah@gmail.com>
2024-06-20 13:24:09 -0700

    fix: readme deployed instead of site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({ site: "https://kargapp.github.io" });
+export default defineConfig({
+  site: "https://kargapp.github.io",
+  base: "portfolio",
+});
